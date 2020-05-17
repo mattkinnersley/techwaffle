3711f596f91934b9e5656eda2e2e8cc900380080
Matt Kinnersley <kinnersleymatt@gmail.com>
2020-05-17 11:49:28 +0100

    add tailwind

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,14 @@ import React from "react";
 import Head from "next/head";
 
 const Home = () => (
-  <div className="container">
+  <div className="flex">
     <Head>
       <title>Tech Waffle | The Tech Blog</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
     <div>
-      <h1>Tech Waffle</h1>
+      <h1 className="text-waffle-400">Tech Waffle</h1>
       <p>Coming soon...</p>
     </div>
   </div>
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: [],
+  theme: {
+    colors: {
+      waffle: "#ffce5c",
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
