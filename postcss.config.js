53a6c1c889bede55cfaaba5ae7a52255561a5c4a
Matt Kinnersley <kinnersleymatt@gmail.com>
2020-05-22 20:08:32 +0100

    purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,21 +1,21 @@
 module.exports = {
   plugins: [
     "tailwindcss",
-    // ...(process.env.NODE_ENV === "production"
-    //   ? [
-    //       [
-    //         "@fullhuman/postcss-purgecss",
-    //         {
-    //           content: [
-    //             "./pages/**/*.{js,jsx,ts,tsx}",
-    //             "./components/**/*.{js,jsx,ts,tsx}",
-    //           ],
-    //           defaultExtractor: (content) =>
-    //             content.match(/[\w-/:]+(?<!:)/g) || [],
-    //         },
-    //       ],
-    //     ]
-    //   : []),
+    ...(process.env.NODE_ENV === "production"
+      ? [
+          [
+            "@fullhuman/postcss-purgecss",
+            {
+              content: [
+                "./src/pages/**/*.{js,jsx,ts,tsx}",
+                "./src/components/**/*.{js,jsx,ts,tsx}",
+              ],
+              defaultExtractor: (content) =>
+                content.match(/[\w-/:]+(?<!:)/g) || [],
+            },
+          ],
+        ]
+      : []),
     "postcss-preset-env",
   ],
 };
