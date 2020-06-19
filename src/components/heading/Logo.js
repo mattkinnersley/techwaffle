7e642640c6502bf6a00a9d5e353ce1f54762079c
Matt Kinnersley <kinnersleymatt@gmail.com>
2020-06-19 13:34:44 +0100

    svg to png

--- src/components/heading/Logo.js
+++ src/components/heading/Logo.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Logo = ({ className }) => (
-  <img className={className} src="/waffle.svg"></img>
+  <img className={className} src="/waffle.png"></img>
 );
 
 export default Logo;
