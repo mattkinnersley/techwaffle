0176ef9a39090d1df9c2c2bc32605807d1b32eb9
Matt Kinnersley <kinnersleymatt@gmail.com>
2020-07-10 17:08:30 +0100

    syntax highlighting

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
-import "../styles.css";
-
+import '../styles.css';
+import '../prism.js';
+import '../prism.css';
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
