9c8e0fdecd195db4c8f098fb89df4e1f70a55315
Matt Kinnersley <kinnersleymatt@gmail.com>
2020-12-17 10:23:50 +0000

    Add email functionality
    Prior to this change, there was no way to subscribe to mailchimp audience
    
    This change adds a form and an api to communicate with mailchimp api.

--- src/components/email/index.js
+++ src/components/email/index.js
@@ -4,31 +4,45 @@ import { useForm } from "react-hook-form";
 
 const EmailForm = () => {
   const { register, handleSubmit, errors } = useForm();
-  const onSubmit = (data) => {
-    console.log(data);
-    axios.post("/api/mailchimp", data);
+  const onSubmit = async (data) => {
+    try {
+      await axios.post("/api/mailchimp", data);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
     <footer className="flex flex-col justify-center items-center pb-16 pt-5 min-w-full bg-waffleLight">
-      <h3 className="text-2xl mt-4 font-quicksand-bold">
-        Want to stay updated?
-      </h3>
-      <p className="mb-4">I'll let you know when I upload a new article.</p>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input
-          className="rounded-md border-2 border-gray-800 p-2 mr-4 focus:outline-none"
-          type="text"
-          placeholder="you@email.com"
-          name="email"
-          ref={register({ required: true, pattern: /^\S+@\S+$/i })}
-        />
+      <div className="flex flex-col justify-center items-center sm:w-8/12 lg:w-6/12 xl:w-4/12">
+        <h3 className="text-2xl mt-4 font-quicksand-bold">
+          Want to stay updated?
+        </h3>
+        <p className="mb-4">I'll let you know when I upload a new article.</p>
+        <form
+          className="flex flex-col justify-center items-center min-w-full"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <div className="flex flex-col items-center mb-4 min-w-full">
+            <input
+              className="min-w-full rounded-md border-2 border-gray-800 p-2 focus:outline-none"
+              type="text"
+              placeholder="you@email.com"
+              name="email"
+              ref={register({
+                required: true,
+                pattern: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/g,
+              })}
+            />
+            {errors.email && <span>Please enter a valid email address</span>}
+          </div>
 
-        <input
-          className="rounded-md border-2 border-gray-800 p-2 bg-white font-quicksand-bold duration-300 thumbnail-shadow transition cursor-pointer focus:outline-none active:shadow-inner"
-          type="submit"
-        />
-      </form>
+          <input
+            className="rounded-md border-2 border-gray-800 p-2 bg-white min-w-full font-quicksand-bold duration-300 thumbnail-shadow transition cursor-pointer focus:outline-none active:shadow-inner"
+            type="submit"
+          />
+        </form>
+      </div>
     </footer>
   );
 };
--- src/pages/api/mailchimp/index.js
+++ src/pages/api/mailchimp/index.js
@@ -7,16 +7,25 @@ client.setConfig({
 
 export default async function handler(req, res) {
   try {
-    const email_address = req.body.email;
-    const response = await client.lists.addListMember(process.env.LIST_ID, {
-      email_address,
-      status: "subscribed",
+    const emailRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/g;
+    const email_address = req?.body?.email;
+    const validEmail = email_address && emailRegex.test(email_address);
+    if (validEmail) {
+      const response = await client.lists.addListMember(process.env.LIST_ID, {
+        email_address,
+        status: "subscribed",
+      });
+      res.status(200).json(response);
+    } else {
+      res.status(400).json({
+        error: "Email validation failed",
+        message: "Please enter a valid email address.",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      error: "Server Error",
+      message: "Sorry, something went wrong, please try again later.",
     });
-    console.log(response);
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "application/json");
-    res.send(response);
-  } catch (e) {
-    console.error(e);
   }
 }
