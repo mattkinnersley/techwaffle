9531d4e533d5d5d76784e4aad38c6de6c1f441d6
Matt Kinnersley <kinnersleymatt@gmail.com>
2020-12-17 12:14:47 +0000

    Fix footer padding

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -4,7 +4,7 @@ import EmailForm from "./email";
 
 const Footer = () => {
   return (
-    <footer className="flex flex-col justify-center items-center pb-16 pt-5 min-w-full bg-waffleLight">
+    <footer className="flex flex-col justify-center items-center text-center pb-16 pt-5 px-5 md:px-10 lg:px-32 min-w-full bg-waffleLight">
       <div className="flex flex-col justify-center items-center sm:w-8/12 lg:w-6/12 xl:w-4/12">
         <h3 className="text-2xl mt-4 font-quicksand-bold">
           Want to stay updated?
