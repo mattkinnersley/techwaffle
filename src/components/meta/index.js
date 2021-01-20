7ce56b4b77a37c9be943f96ba1b8f1d1f9bf6ed2
Matt Kinnersley <33697516+KinnersleyM@users.noreply.github.com>
2021-01-20 12:59:37 +0000

    Update index.js

--- src/components/meta/index.js
+++ src/components/meta/index.js
@@ -12,11 +12,11 @@ const Meta = ({ title, description }) => {
       <meta name="twitter:description" content={description} />
       <meta
         name="twitter:image"
-        content="https://www.techwaffle.dev/waffle-400x400.png"
+        content="https://www.techwaffle.dev/techwaffle-heading.png"
       />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content="https://www.techwaffle.dev/waffle-200x200.png" />
+      <meta property="og:image" content="https://www.techwaffle.dev/techwaffle-heading.png" />
       <meta name="description" content={description} />
     </Head>
   );
