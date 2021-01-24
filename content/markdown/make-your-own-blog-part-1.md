b1e4d006d1fe55c97b4ea98e541217537f99aa49
Robin278 <Robin278@c02vd2u2hv2h.lan>
2021-01-24 12:40:25 +0000

    post-series

--- content/markdown/make-your-own-blog-part-1.md
+++ content/markdown/make-your-own-blog-part-1.md
@@ -2,6 +2,7 @@
 title: "Make Your Own Blog - Part 1"
 subtitle: "Making a Blog with NextJS, Mailchimp and Google Analytics"
 excerpt: "Starting a blog is a great way to get your thoughts down on paper and out into the world. It helps to consolidate ideas, document your learning and then share it all with others."
+series: "make-your-own-blog-part-1,make-your-own-blog-part-2,make-your-own-blog-part-3"
 date: "2021-01-20T10:10:01.152Z"
 author: "Matt Kinnersley"
 ---
--- src/pages/posts/[slug].js
+++ src/pages/posts/[slug].js
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import Link from "next/link";
+import PostSeries from "../../components/post-series";
 import { getPostBySlug, markdownToHtml, getAllPosts } from "../../utils";
 
 export default function Post({ post }) {
@@ -13,20 +13,13 @@ export default function Post({ post }) {
         <div className="text-center">
           <h1 className="text-5xl font-extrabold">{post.title}</h1>
         </div>
-        {post.seriesPaths && (
-          <div className="bg-waffleLight dark:bg-gray-800">
-            {post.seriesPaths.map(path => (
-              <Link as={path} href="/posts/[slug]">
-                <a>
-                  <div className="rounded-md border-2 border-gray-800 dark:border-yellow-50 flex flex-col md:flex-row items-center justify-between p-4 thumbnail-shadow text-center m-4">
-                    <h3 className="font-quicksand-bold">{path}</h3>
-                  </div>
-                </a>
-              </Link>
-            ))}
-          </div>
+        {post.seriesPosts && (
+          <PostSeries
+            series={post.seriesPosts}
+            currentPostSlug={post.slug}
+          ></PostSeries>
         )}
-        
+
         <div
           className="markdown min-w-full"
           dangerouslySetInnerHTML={{ __html: post.htmlContent }}
@@ -37,10 +30,24 @@ export default function Post({ post }) {
 }
 
 export async function getStaticProps({ params: { slug } }) {
-  const { title, subtitle, date, series, author, content, excerpt } = getPostBySlug(
-    slug
-  );
-  const seriesPaths = series?.split(",").map(slug => `/posts/${slug}`) || ''
+  const {
+    title,
+    subtitle,
+    date,
+    series,
+    author,
+    content,
+    excerpt,
+  } = getPostBySlug(slug);
+  const seriesPosts =
+    series?.split(",").map((slug, index) => {
+      const { title } = getPostBySlug(slug);
+      if (!title) {
+        return { title: `Part ${index + 1} coming soon` };
+      }
+      return { title, slug };
+    }) || "";
+
   const htmlContent = await markdownToHtml(content || "");
   return {
     props: {
@@ -49,7 +56,7 @@ export async function getStaticProps({ params: { slug } }) {
       post: {
         title,
         date,
-        seriesPaths,
+        seriesPosts,
         author,
         htmlContent,
         excerpt,
