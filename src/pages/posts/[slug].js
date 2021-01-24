24503c5bf05d0df8bf56eb099de765ba135ec098
Matt Kinnersley <33697516+KinnersleyM@users.noreply.github.com>
2021-01-24 09:52:32 +0000

    Update [slug].js

--- src/pages/posts/[slug].js
+++ src/pages/posts/[slug].js
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router";
+import Link from "next/link";
 import { getPostBySlug, markdownToHtml, getAllPosts } from "../../utils";
 
 export default function Post({ post }) {
