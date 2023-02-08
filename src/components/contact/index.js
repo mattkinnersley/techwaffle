0773865a42b3f211e01d01e9333093914bbd25b3
Matt Kinnersley <33697516+mattkinnersley@users.noreply.github.com>
2023-02-08 12:18:02 +0000

    Update contact details

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -8,12 +8,12 @@ const Contact = () => {
       <h3 className="text-2xl mt-2 mb-2 font-quicksand-bold">Get in touch</h3>
       <div className="flex min-w-full justify-around">
         <Button
-          href="https://www.twitter.com/techwaffler"
+          href="https://www.twitter.com/mattkinnersley_"
           aria-label="Go to twitter"
         >
           <Twitter />
         </Button>
-        <Button href="mailto:matt@techwaffle.dev" aria-label="Send me an email">
+        <Button href="mailto:hello@mattkinnersley.com" aria-label="Send me an email">
           <Mail />
         </Button>
       </div>
